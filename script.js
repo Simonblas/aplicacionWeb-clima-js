665f50949e949deb2987ba4720d0b897e2d33956
Simon Blas <simoblas08@gmail.com>
2024-09-09 13:12:49 -0300

    agregamos los comentarios al trabajo

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.getElementById('searchButton').addEventListener('click', () => {
     const city = document.getElementById('cityInput').value //guardo en una constante el valor del cityinput del html
     if (city) { //si city es creada, es decir, tiene un valor
         fetchWeather(city)//llamar a la api que nos de la informacion del clima de esa ciudad recibida mediante la funcion creada:\
-        fetchWeather(city)
     } else {
         alert('ingrese una ciudad válida')
     }
@@ -17,14 +16,15 @@ document.getElementById('searchButton').addEventListener('click', () => {
 function fetchWeather(city) {//esta funcion llama a la api
     fetch(`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`) //este llamado a la api devuelve una promesa
         .then(data => data.json()) //con un .then recibimos esa promesa devuelta y la transformamos en un objeto tipo json
-        .then(data => showWeatherData(data) //con este .then recibimos el objeto json y lo mostramos por consola
+        .then(data => showWeatherData(data) //con este .then recibimos el objeto json mediante el parametro de data e invocamos la funcion de showWeatherData pasandole el json como parametro
         )
 }
 
-function showWeatherData(data) {
-    const divResponseData = document.getElementById('responseData')
-    divResponseData.innerHTML = ''
+function showWeatherData(data) { //funcion que se invocara desde el dom
+    const divResponseData = document.getElementById('responseData') //traigo del html un div
+    divResponseData.innerHTML = '' //vacio el div
 
+    //guardo en constantes los valores que quiero del objeto json que me trajo el llamado a la api
     const cityName = data.name;
     const countryName = data.sys.country;
     const temp = data.main.temp;
@@ -32,6 +32,8 @@ function showWeatherData(data) {
     const description = data.weather[0].description;
     const icon = data.weather[0].icon;
 
+    //creo un elemento en el html para cada informacion que quiero mostrar del json de la api y guardo esos elementos creados en una constante
+    //luego de eso, a los elementos creados les agrego como contenido la info de la api
     const cityInfo = document.createElement('h2');
     cityInfo.textContent = `${cityName}, ${countryName}`
 
@@ -47,6 +49,7 @@ function showWeatherData(data) {
     const descriptionInfo = document.createElement('p')
     descriptionInfo.textContent = `La descripcion meteorológica es ${description}`
 
+    //al div del html le asigno los elementos creados como hijos, para que el div sea un contenedor de estos elementos
     divResponseData.appendChild(cityInfo)
     divResponseData.appendChild(tempInfo)
     divResponseData.appendChild(humidityInfo)
